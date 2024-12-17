d06a7348c5dbe5f1765e4c4c1fdbb635a0451ef5
boel78 <95147718+boel78@users.noreply.github.com>
2024-12-17 16:24:18 +0100

    Initial commit

---
+++ cvprojekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cvprojekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
