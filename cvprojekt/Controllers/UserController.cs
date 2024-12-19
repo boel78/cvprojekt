e072b87f03cdb89a75e633246fda25a99d181b3e
boel78 <95147718+boel78@users.noreply.github.com>
2024-12-19 09:39:23 +0100

    En enkel redigera användare funktion

---
+++ cvprojekt/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using cvprojekt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cvprojekt.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly CvDbContext _ctx;
+
+        public UserController(CvDbContext ctx) {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            User user = _ctx.Users.Find(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, User newUser)
+        {
+            User oldUser = _ctx.Users.Find(id);
+
+            oldUser.Name = newUser.Name;
+            oldUser.Email = newUser.Email;
+            oldUser.Password = newUser.Password;
+            oldUser.IsPrivate = newUser.IsPrivate;
+            oldUser.IsActive = newUser.IsActive;
+
+            _ctx.SaveChanges();
+            return RedirectToAction("Home", "Index");
+        }
+
+    }
+}
