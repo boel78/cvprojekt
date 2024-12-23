29fbace9a752ed1e747415180fba60ccbbab80d7
boel78 <95147718+boel78@users.noreply.github.com>
2024-12-23 14:25:32 +0100

    fixade konflikt

--- cvprojekt/Controllers/HomeController.cs
+++ cvprojekt/Controllers/HomeController.cs
@@ -41,17 +41,6 @@ namespace cvprojekt.Controllers
 
 
             IndexViewModel im = new IndexViewModel();
-
-            IQueryable<Cv> cvList = (from Cv in _context.Cvs where Cv.OwnerNavigation.IsActive == false select Cv).Include(c => c.Educations)
-                .ThenInclude(e => e.Skills).Include(c => c.OwnerNavigation);
-
-            IQueryable<Project> projectList = (from Project in _context.Projects
-                    select Project)
-                .OrderBy(p => p.CreatedDate)
-                .Take(3);
-
-            im.projects = projectList;
-            im.cvs = cvList;
             
             var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
             ViewBag.UnreadMessagesCount = userId != null ? await _messageService.GetUnreadMessagesCountAsync(userId) : 0;
