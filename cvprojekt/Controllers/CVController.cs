2d52c41177901b9d3b16f41307f1140afe65d084
boel78 <95147718+boel78@users.noreply.github.com>
2024-12-23 10:52:06 +0100

    123

--- cvprojekt/Controllers/CVController.cs
+++ cvprojekt/Controllers/CVController.cs
@@ -1,24 +1,24 @@
-﻿using cvprojekt.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿//using cvprojekt.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
 
-namespace cvprojekt.Controllers
-{
-    public class CVController : Controller
-    {
-        private readonly CvDbContext _dbContext;
+//namespace cvprojekt.Controllers
+//{
+//    public class CVController : Controller
+//    {
+//        private readonly CvDbContext _dbContext;
 
-        public CVController(CvDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+//        public CVController(CvDbContext dbContext)
+//        {
+//            _dbContext = dbContext;
+//        }
 
 
-        public IActionResult Index(string projekt)
-        {
-            List<Cv> cvs = _dbContext.Cvs.Where(c => c.Projects.Any(p => p.Title.Contains(projekt))).ToList();
+//        public IActionResult Index(string projekt)
+//        {
+//            List<Cv> cvs = _dbContext.Cvs.Where(c => c.Projects.Any(p => p.Title.Contains(projekt))).ToList();
 
-            return View(cvs);
-        }
-    }
-}
+//            return View(cvs);
+//        }
+//    }
+//}
